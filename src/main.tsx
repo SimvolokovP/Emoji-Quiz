32e44ed57c3e42de04e7e9e0fbcb2add3f91fd48
Simvolokov Pavel <psimvolokov@gmail.com>
2025-03-11 13:17:39 +0300

    add gh pages

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,11 @@ import "./styles/normalize.css";
 import "./styles/index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-import './i18n';
+import "./i18n";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Emoji-Quiz">
       <App />
     </BrowserRouter>
   </StrictMode>
